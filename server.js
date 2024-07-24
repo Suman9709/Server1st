cb29bdaa457fa4eb31e88533b21f0ad1533cc692
suman kumar <suman kumar>
2024-07-24 16:33:13 +0530

    server backend 1st day file submitting

---
+++ server.js
@@ -0,0 +1,25 @@
+// import http from 'http';
+let http = require("http");
+let fs = require("fs");
+
+console.log("starting server");
+http.createServer((req, res) => {
+    // console.log({Headers: req.headers})
+    // res.write('hello suman');
+    // console.log("sending response")
+    // res.end();
+    fs.readFile('response.html', (err, data) => {
+        console.log(req.url);
+        if (!err) {
+            res.writeHead(200, {
+                "Content-Type": "text/html"
+            });
+            res.write(data);
+            return res.end();
+        }
+        res.write('error while reading file');
+        return res.end();
+    })
+
+})
+    .listen(6060)
